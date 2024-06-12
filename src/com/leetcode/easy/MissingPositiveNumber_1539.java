ae906bb125d940950a1ed64ea2696dbadb0df3e9
Sydur Rahman <rahmansydur@hotmail.com>
2024-06-12 11:57:31 +0100

    kth Missing Positive number leetcode 1539 -To Find the number of missing elements/number from a point of array (mid), We have to do use this {arr[index]-index-1}. - ans+k gives use the missing number at k.

---
+++ src/com/leetcode/easy/MissingPositiveNumber_1539.java
@@ -0,0 +1,31 @@
+package com.leetcode.easy;
+
+import java.util.ArrayList;
+
+public class MissingPositiveNumber_1539 {
+    public static void main(String[] args) {
+        int arr[]= {2,3,4,7,11};
+        System.out.println(kthPositive(arr,5));
+    }
+
+    public static int kthPositive(int [] arr,int k){
+        int start= 0;
+        int end=arr.length-1;
+        int ans=0;
+
+        while (start<=end){
+            int mid= start+(end-start)/2;
+            int countMissing=arr[mid]-mid-1;
+            if(countMissing>k){
+                ans=mid;
+                end=mid-1;
+            }   else {
+                start=mid+1;
+            }
+        }
+
+
+        return ans+k;
+    }
+
+}
